2321939ed1057d02012663794899efff619474c0
Dbentes <dmrsn.diego@gmail.com>
2019-09-04 08:54:30 -0400

    Novos Arquivos

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,  /*velocidade do slide*/
+    slidesToShow: 1, /*Quantos slide vai mostrar cada vez que passa*/
+    adaptiveHeight: true
+
+});
